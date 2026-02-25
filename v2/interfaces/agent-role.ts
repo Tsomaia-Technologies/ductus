efa44516eb959fc61789d9c9e030ac4eac1318ee
Tornike Tsomaia <ttsomaia@emis.ge>
2026-02-25 22:14:25 +0400

    Changes

--- v2/interfaces/agent-role.ts
+++ v2/interfaces/agent-role.ts
@@ -1,4 +1,14 @@
-export interface AgentRole<TContext extends object> {
-  type: string
+export type AgentType =
+  | 'planner'
+  | 'task-creator' // or 'delegator'
+  | 'task-auditor'
+  | 'engineer'
+  | 'reviewer'
+  | 'auditor'
+
+export interface AgentRole<TContext extends object, TOutput> {
+  type: AgentType
+  allowedTools(): string[]
   persona(context: TContext): string
+  parse(response: string): TOutput
 }
