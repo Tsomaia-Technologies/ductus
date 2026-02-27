35c62519aa9edf20f41faf3dec5a77f12cb33e0e
Tornike Tsomaia <ttsomaia@emis.ge>
2026-02-27 16:36:02 +0400

    Changes

--- v2/interfaces/agent-dispatcher.ts
+++ v2/interfaces/agent-dispatcher.ts
@@ -1,11 +1,13 @@
+import { AgentContext } from './agent-context.js'
 import { AgentRole } from './agent-role.js'
 import { OutputEventStream } from './output-event-stream.js'
 
 export interface AgentDispatcher {
   process<TContext extends object>(
     input: string,
-    role: AgentRole<TContext>,
+    role: AgentRole<TContext, any>,
+    context?: AgentContext
   ): Promise<OutputEventStream>
 
-  terminate(role: AgentRole<object>): void
+  terminate(role: AgentRole<any, any>): void
 }
--- v2/interfaces/event-processor.ts
+++ v2/interfaces/event-processor.ts
@@ -2,5 +2,5 @@ import { InputEventStream } from './input-event-stream.js'
 import { OutputEventStream } from './output-event-stream.js'
 
 export interface EventProcessor {
-  process(stream: InputEventStream): AsyncIterable<OutputEventStream>
+  process(stream: InputEventStream): OutputEventStream
 }
