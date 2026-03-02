579b87ee1f4d3f69812f9f7588b5f5fbade36ebc
Tornike Tsomaia <ttsomaia@emis.ge>
2026-03-02 21:51:09 +0400

    Manual: changes

---
+++ src/manual/core/dependency-container.ts
@@ -0,0 +1,20 @@
+import { Type } from '../interfaces/event-generator.js'
+import { DependencyContainer } from '../interfaces/dependency-container.js'
+
+export class DefaultDependencyInjector implements DependencyContainer {
+  private instances = new Map<Type, InstanceType<Type>>()
+
+  register<T extends Type>(type: T, instance: InstanceType<Type>): void {
+    this.instances.set(type, instance)
+  }
+
+  use<T extends Type, U extends InstanceType<T> = InstanceType<T>>(type: T): U {
+    const instance = this.instances.get(type)
+
+    if (!instance) {
+      throw new TypeError(`No registry found for type ${type.name}`)
+    }
+
+    return instance
+  }
+}
---
+++ src/manual/interfaces/dependency-container.ts
@@ -0,0 +1,6 @@
+import { Type } from './event-generator.js'
+
+export interface DependencyContainer {
+  register<T extends Type>(type: T, instance: InstanceType<Type>): void
+  use<T extends Type, U extends InstanceType<T> = InstanceType<T>>(type: T): U
+}
