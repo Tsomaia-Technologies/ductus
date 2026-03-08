2fd35adb2fa63378a2c18234e92be3453f01e41a
Tornike Tsomaia <ttsomaia@emis.ge>
2026-03-08 10:24:40 +0400

    Changes

--- src/core/mutex.ts
+++ src/core/mutex.ts
@@ -1,27 +1,24 @@
-import { LinkedList } from './linked-list.js'
+import { DefaultDeferrer } from './default-deferrer.js'
 
 export class Mutex {
-    private isLocked = false
-    private readonly waiters = new LinkedList<() => void>()
+  private isLocked = false
+  private readonly deferrer = new DefaultDeferrer()
 
-    async lock<T>(execute: () => Promise<T>): Promise<T> {
-        if (this.isLocked) {
-            await new Promise<void>(resolve => {
-                this.waiters.insertLast(resolve)
-            })
-        }
-
-        this.isLocked = true
-
-        try {
-            return await execute()
-        } finally {
-            const nextWaiter = this.waiters.removeFirst()
-            if (nextWaiter) {
-                nextWaiter()
-            } else {
-                this.isLocked = false
-            }
-        }
+  async lock<T>(execute: () => Promise<T>): Promise<T> {
+    if (this.isLocked) {
+      await this.deferrer.sleep()
     }
+
+    this.isLocked = true
+
+    try {
+      return await execute()
+    } finally {
+      if (this.deferrer.isWaiting()) {
+        this.deferrer.wakeUpNext()
+      } else {
+        this.isLocked = false
+      }
+    }
+  }
 }
